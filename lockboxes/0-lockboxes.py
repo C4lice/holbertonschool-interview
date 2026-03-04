ff7a22c5999fdc7b3531addfb8b9900cf5e7eb94
C4lice <laura.aupetit002@gmail.com>
2026-03-04 10:43:17 +0100

    Amélioration de la documentation de la fonction canUnlockAll

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
+"""
+Module contenant la fonction canUnlockAll
+"""
+
+
 def canUnlockAll(boxes):
+    """
+    Détermine si toutes les boîtes peuvent être ouvertes.
+
+    :param boxes : liste de listes contenant des clés
+    :return : True si toutes les boîtes peuvent être ouvertes, False sinon
+    """
     n = len(boxes)
-    opened = set([0])   # boîte 0 déjà ouverte
-    stack = [0]         # pile pour explorer
+
+    # Set pour garder les boîtes déjà ouvertes
+    opened = set([0])
+
+    # Pile pour parcourir les boîtes (DFS)
+    stack = [0]
 
     while stack:
-        current = stack.pop()
+        current_box = stack.pop()
 
-        for key in boxes[current]:
+        for key in boxes[current_box]:
+            # Vérifie que la clé correspond à une boîte existante
             if key < n and key not in opened:
                 opened.add(key)
                 stack.append(key)
