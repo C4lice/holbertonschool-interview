0251e0403d0e6c53a87774f5b64e13f16ebe6d81
C4lice <laura.aueptit002@gmail.com>
2026-03-06 14:11:45 +0100

    importation du projet  Linked list palindrome

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,68 @@
+#include "lists.h"
+
+/**
+ * reverse_list - reverse a singly linked list
+ * @head: pointer to head
+ * Return: new head of reversed list
+ */
+listint_t *reverse_list(listint_t *head)
+{
+    listint_t *prev = NULL;
+    listint_t *next;
+
+    while (head)
+    {
+        next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
+
+    return (prev);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to pointer to head
+ * Return: 1 if palindrome, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+    listint_t *slow, *fast;
+    listint_t *first_half, *second_half, *rev_second;
+    int palindrome = 1;
+
+    if (!head || !*head || !(*head)->next)
+        return (1); /* empty or single node list is palindrome */
+
+    slow = fast = *head;
+
+    /* Find middle */
+    while (fast->next && fast->next->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    /* Reverse second half */
+    second_half = reverse_list(slow->next);
+
+    /* Compare first half and reversed second half */
+    first_half = *head;
+    rev_second = second_half;
+    while (rev_second)
+    {
+        if (first_half->n != rev_second->n)
+        {
+            palindrome = 0;
+            break;
+        }
+        first_half = first_half->next;
+        rev_second = rev_second->next;
+    }
+
+    /* Restore the list */
+    slow->next = reverse_list(second_half);
+
+    return (palindrome);
+}
