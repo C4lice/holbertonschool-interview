ae5ba9b21c98c4b66ae76ac57b8817d84362ada3
C4lice <laura.aupetit002@gmail.com>
2026-04-10 16:50:03 +0200

    Refactor the `menger` function to simplify the drawing of the Menger sponge by incorporating the hole-checking logic directly into the main loop.

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -1,50 +1,45 @@
-#include <stdio.h>
-#include <math.h>
 #include "menger.h"
 
 /**
- * is_hole - check if a position should be empty
- * @x: row index
- * @y: column index
+ * menger - function that draws a 2D Menger Sponge
  *
- * Return: 1 if position is a hole, 0 otherwise
- */
-int is_hole(int x, int y)
-{
-	while (x > 0 || y > 0)
-	{
-		if (x % 3 == 1 && y % 3 == 1)
-			return (1);
-		x /= 3;
-		y /= 3;
-	}
-	return (0);
-}
-
-/**
- * menger - draws a 2D Menger Sponge
- * @level: level of the sponge
+ * @level: the number of level
  *
- * Return: nothing
+ * Return: NOTHING
  */
 void menger(int level)
 {
-	int size, i, j;
+	int x;
+	int y;
+	int size = (int)pow(3, level);
+	int hole = 0;
 
-	if (level < 0)
-		return;
-
-	size = pow(3, level);
-
-	for (i = 0; i < size; i++)
+	if (level >= 0)
 	{
-		for (j = 0; j < size; j++)
+		for (x = 0; x < size; x++)
 		{
-			if (is_hole(i, j))
-				printf(" ");
-			else
-				printf("#");
+			for (y = 0; y < size ; y++)
+			{
+				hole = 0;
+				int tx = x;
+				int ty = y;
+
+				while (tx > 0 || ty > 0)
+				{
+					if (tx % 3 == 1 && ty % 3 == 1)
+					{
+						hole = 1;
+						break;
+					}
+					tx /= 3;
+					ty /= 3;
+				}
+				if (hole)
+					printf(" ");
+				else
+					printf("#");
+			}
+			printf("\n");
 		}
-		printf("\n");
 	}
 }
