224a23c1f65d6878f59ac15d8531338c54140312
C4lice <laura.aupetit002@gmail.com>
2026-06-05 20:15:29 +0200

    Refactor la fonction print_array pour améliorer la lisibilité et corriger le type de paramètre, et renommer la fonction de recherche récursive pour une meilleure clarté.

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -1,78 +1,62 @@
-#include "search_algos.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
 
 /**
- * print_array - Affiche les éléments du sous-tableau
- * @array: pointeur vers le début du sous-tableau
- * @size: nombre d'éléments à afficher
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ *
+ * Return: Nothing
  */
-static void print_array(int *array, size_t size)
+void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i = 0;
 
-    printf("Searching in array: ");
-    for (i = 0; i < size; i++)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-    }
-    printf("\n");
+	printf("Searching in array: ");
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
 
-/**
- * advanced_binary_recursive - fonction récursive interne
- * @array: pointeur vers le début du sous-tableau actuel
- * @left: index gauche (dans le tableau original)
- * @right: index droit (dans le tableau original)
- * @value: valeur à chercher
- *
- * Return: index du PREMIER occurrence, ou -1
- */
-static int advanced_binary_recursive(int *array, size_t left,
-                                      size_t right, int value)
+
+int recursive(int *array, int left, int right, int value)
 {
-    size_t mid;
+	int mid;
 
-    /* Condition d'arrêt : sous-tableau vide */
-    if (left > right)
-        return (-1);
+	if (left > right)
+		return (-1);
 
-    mid = left + (right - left) / 2;
+	print_array(array + left, right - left + 1);
 
-    /* Affiche le sous-tableau actuel */
-    print_array(array + left, right - left + 1);
+	mid = left + (right - left) / 2;
 
-    /* Si le milieu EST la valeur cherchée */
-    if (array[mid] == value)
-    {
-        /* C'est déjà le tout premier élément du sous-tableau ? */
-        if (mid == left)
-            return (mid);
-        /* Sinon, on cherche encore plus à gauche */
-        return (advanced_binary_recursive(array, left, mid, value));
-    }
+	if (array[mid] == value)
+	{
+		if (mid == left || array[mid - 1] != value)
+			return (mid);
 
-    /* Valeur cherchée est à droite */
-    if (array[mid] < value)
-        return (advanced_binary_recursive(array, mid + 1, right, value));
+		return (recursive(array, left, mid, value));
+	}
 
-    /* Valeur cherchée est à gauche */
-    return (advanced_binary_recursive(array, left, mid - 1, value));
+	if (array[mid] < value)
+		return (recursive(array, mid + 1, right, value));
+
+	return (recursive(array, left, mid, value));
 }
 
-/**
- * advanced_binary - Point d'entrée public
- * @array: tableau trié d'entiers
- * @size: nombre d'éléments
- * @value: valeur à chercher
- *
- * Return: index du premier occurrence de value, ou -1
- */
+
 int advanced_binary(int *array, size_t size, int value)
 {
-    if (array == NULL || size == 0)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    return (advanced_binary_recursive(array, 0, size - 1, value));
+	return (recursive(array, 0, size - 1, value));
 }
